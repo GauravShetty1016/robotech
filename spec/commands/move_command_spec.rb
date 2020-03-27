41d78c313556fe609f7965faaba6646f2760cca6
Gaurav Shetty <1016.gaurav@gmail.com>
2020-03-28 09:45:00 +1100

    Updating the spec file to test the new valid action methods and flow.

--- spec/commands/move_command_spec.rb
+++ spec/commands/move_command_spec.rb
@@ -20,6 +20,32 @@ RSpec.describe Robotech::Commands::MoveCommand do
     expect(command.perform).to be false
   end
 
+  it "should return the default valid action" do
+    command = Robotech::Commands::MoveCommand.new(command: ["move"], position: { x: 1, y: 2, facing_index: 0 }, orientations: ["north", "east", "south", "west"], ruleset: double({ valid_position?: true }))
+    expect(command.valid_action).to eq("move")
+  end
+
+  it "should return new valid action when the action is changed" do
+    command = Robotech::Commands::MoveCommand.new(command: ["move"], position: { x: 1, y: 2, facing_index: 0 }, orientations: ["north", "east", "south", "west"], ruleset: double({ valid_position?: true }))
+    command.valid_action = "moving"
+    expect(command.valid_action).to eq("moving")
+  end
+
+  it "should process the new command when the action is changed" do
+    command = Robotech::Commands::MoveCommand.new(command: ["moving"], position: { x: 1, y: 2, facing_index: 0 }, orientations: ["north", "east", "south", "west"], ruleset: double({ valid_position?: true }))
+    command.valid_action = "moving"
+    expect(command.valid_action).to eq("moving")
+    expect(command.perform).to eq({ x: 1, y: 3, facing_index: 0 })
+    expect(command.position).to eq({ x: 1, y: 3, facing_index: 0 })
+  end
+
+  it "should process the new command when the action is changed in the initializer" do
+    command = Robotech::Commands::MoveCommand.new(command: ["moving"], valid_action: "moving", position: { x: 1, y: 2, facing_index: 0 }, orientations: ["north", "east", "south", "west"], ruleset: double({ valid_position?: true }))
+    expect(command.valid_action).to eq("moving")
+    expect(command.perform).to eq({ x: 1, y: 3, facing_index: 0 })
+    expect(command.position).to eq({ x: 1, y: 3, facing_index: 0 })
+  end
+
   it "should move in the correct direction if the move is valid" do
     command = Robotech::Commands::MoveCommand.new(command: ["move"], position: { x: 1, y: 2, facing_index: 0 }, orientations: ["north", "east", "south", "west"], ruleset: double({ valid_position?: true }))
     expect(command.perform).to eq({ x: 1, y: 3, facing_index: 0 })
