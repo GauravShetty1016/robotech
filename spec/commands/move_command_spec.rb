757ae64b79b382c9ebbfb24874852054928f7691
Gaurav Shetty <1016.gaurav@gmail.com>
2020-03-28 09:54:17 +1100

    Removing unecessary orientation param from the new class initialization.

--- spec/commands/move_command_spec.rb
+++ spec/commands/move_command_spec.rb
@@ -2,17 +2,17 @@ require_relative "../../app/commands/move_command"
 
 RSpec.describe Robotech::Commands::MoveCommand do
   it "should return false when the position is invalid" do
-    command = Robotech::Commands::MoveCommand.new(command: ["move"], position: nil, orientations: ["a"], ruleset: double({ valid_position?: true }))
+    command = Robotech::Commands::MoveCommand.new(command: ["move"], position: nil, ruleset: double({ valid_position?: true }))
     expect(command.perform).to be false
   end
 
   it "should return false when the command is not move" do
-    command = Robotech::Commands::MoveCommand.new(command: ["moving"], position: nil, orientations: ["a"], ruleset: double({ valid_position?: true }))
+    command = Robotech::Commands::MoveCommand.new(command: ["moving"], position: nil, ruleset: double({ valid_position?: true }))
     expect(command.perform).to be false
   end
 
   it "should raise an error when the command is nil" do
-    expect { Robotech::Commands::MoveCommand.new(command: nil, position: { x: 1, y: 2, facing_index: 1 }, orientations: ["a"], ruleset: double({ valid_position?: true })) }.to raise_error(ArgumentError)
+    expect { Robotech::Commands::MoveCommand.new(command: nil, position: { x: 1, y: 2, facing_index: 1 }, ruleset: double({ valid_position?: true })) }.to raise_error(ArgumentError)
   end
 
   it "should return false if the move is invalid" do
